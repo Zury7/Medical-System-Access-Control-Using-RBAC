4396c1177087d904bfc48271d63eeb7d1f183868
VishvadiniRavihari <120405053+Zury7@users.noreply.github.com>
2023-11-28 14:56:44 +0530

    option list updated

--- Terminal.java
+++ Terminal.java
@@ -31,20 +31,55 @@ controller.readPatientdataFile();
 
 if (active_user_AL == 0){
     System.out.println("Welcome to the Medical System Patient Access Portal !");
+    Scanner scanner1 = new Scanner(System.in);
+    System.out.println("Choose your option to proceed with:");
+    System.out.println(" 1: View my Personal Details");
+    System.out.println(" 2: Edit my Personal Details");
+    System.out.println(" 3: View my Sick Details ");
+    System.out.println(" 4: View my Drug Prescriptions");
+    System.out.println(" 5: View my Lab Report Prescriptions");
+    String input = scanner1.nextLine();
 }
 else if (active_user_AL == 1){
     System.out.println("Welcome to the Medical System Laboratory Staff Access Portal !");
+    Scanner scanner1 = new Scanner(System.in);
+    System.out.println("Choose your option to proceed with:");
+    System.out.println(" 1: View A Pateint's Sickness Details");
+    System.out.println(" 1: View A Pateint's Lab Report Prescription Details");
+    String input = scanner1.nextLine();
+   
 }
 else if (active_user_AL == 2){
     System.out.println("Welcome to the Medical System Nurse Staff Access Portal !");
+    System.out.println("Choose your option to proceed with:");
+    System.out.println(" 1: View A Pateint's Perosnal Details");
+    System.out.println(" 2: View A Patient's Sickness Details");
+    System.out.println(" 3: Edit A Patient's Sickness Details");
+    System.out.println(" 4: View A Patient's Drug Prscriptions");
+    System.out.println(" 5: View A Patient's Lab Report Prescriptions");
 }
 else if (active_user_AL == 3){
     System.out.println("Welcome to the Medical System Medical Officer Staff Access Portal !");
+    System.out.println("Choose your option to proceed with:");
+    System.out.println(" 1: View A Pateint's Perosnal Details");
+    System.out.println(" 2: View A Patient's Sickness Details");
+    System.out.println(" 3: Edit A Patient's Sickness Details");
+    System.out.println(" 4: View A Patient's Drug Prscriptions");
+    System.out.println(" 5: Edit A Patient's Drug Prscriptions");
+    System.out.println(" 6: View A Patient's Lab Report Prescriptions");
+    System.out.println(" 7: Edit A Patient's Drug Prscriptions");
 }
 else if (active_user_AL == 4){
     System.out.println("Welcome to the Medical System Administrator Access Portal !");
+    System.out.println("Choose your option to proceed with:");
+    System.out.println(" 1: View A Pateint's Perosnal Details");
+    System.out.println(" 2: Edit A Pateint's Perosnal Details");
+    System.out.println(" 2: View A Patient's Sickness Details");
+    System.out.println(" 3: Edit A Patient's Sickness Details");
+    System.out.println(" 4: View A Patient's Drug Prscriptions");
+    System.out.println(" 5: Edit A Patient's Drug Prscriptions");
+    System.out.println(" 6: View A Patient's Lab Report Prescriptions");
+    System.out.println(" 7: Edit A Patient's Drug Prscriptions");
 }
-
-
 }
 }
